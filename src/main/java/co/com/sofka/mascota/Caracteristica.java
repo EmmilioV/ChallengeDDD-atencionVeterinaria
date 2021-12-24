e87c60e487455e4295d971ebeea9b964ef02446c
EmmilioV <68575969+EmmilioV@users.noreply.github.com>
2021-12-24 07:59:56 -0500

    Propiedades y comportamientos de entidades del agregado Mascota

--- src/main/java/co/com/sofka/mascota/Caracteristica.java
+++ src/main/java/co/com/sofka/mascota/Caracteristica.java
@@ -1,10 +1,45 @@
 package co.com.sofka.mascota;
 
 import co.com.sofka.domain.generic.Entity;
-import co.com.sofka.mascota.values.CaracteristicaId;
+import co.com.sofka.mascota.values.*;
+
+import java.util.Objects;
 
 public class Caracteristica extends Entity<CaracteristicaId> {
-    public Caracteristica(CaracteristicaId entityId) {
+    private Raza raza;
+    private Peso peso;
+    private Tamanio tamanio;
+    private Color color;
+
+    public Caracteristica(CaracteristicaId entityId, Raza raza, Peso peso, Tamanio tamanio, Color color) {
         super(entityId);
+        this.raza = raza;
+        this.peso = peso;
+        this.tamanio = tamanio;
+        this.color = color;
+    }
+
+    public void actualizarPeso(Peso peso){
+        this.peso = Objects.requireNonNull(peso, "El peso no debe ser nulo");
+    }
+
+    public void actualizarTamanio(Tamanio tamanio){
+        this.tamanio = Objects.requireNonNull(tamanio, "El tamaño no debe ser nulo");
+    }
+
+    public Raza raza() {
+        return raza;
+    }
+
+    public Peso peso() {
+        return peso;
+    }
+
+    public Tamanio tamanio() {
+        return tamanio;
+    }
+
+    public Color color() {
+        return color;
     }
 }
--- src/main/java/co/com/sofka/mascota/Duenio.java
+++ src/main/java/co/com/sofka/mascota/Duenio.java
@@ -1,10 +1,42 @@
 package co.com.sofka.mascota;
 
 import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.generic.Edad;
+import co.com.sofka.generic.Nombre;
 import co.com.sofka.mascota.values.DuenioId;
+import co.com.sofka.mascota.values.Telefono;
+
+import java.util.Objects;
 
 public class Duenio extends Entity<DuenioId> {
-    public Duenio(DuenioId entityId) {
+    private Nombre nombre;
+    private Telefono telefono;
+    private Edad edad;
+
+    public Duenio(DuenioId entityId, Nombre nombre, Telefono telefono, Edad edad) {
         super(entityId);
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    public void actualizarTelefono(Telefono telefono){
+        this.telefono = Objects.requireNonNull(telefono, "El telefono no debe ser nulo");
+    }
+
+    public void actualizarEdad(Edad edad){
+        this.edad = Objects.requireNonNull(edad, "La edad no debe ser nula");
+    }
+
+    public Nombre nombre() {
+        return nombre;
+    }
+
+    public Telefono telefono() {
+        return telefono;
+    }
+
+    public Edad edad() {
+        return edad;
     }
 }
